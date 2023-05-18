db9bdf1d4d3baabd22dc73db1496bef4d6325b81
mariaabr <maria.abrunhosa@ua.pt>
2023-05-18 15:09:34 +0100

    new src commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import { Dropdown } from 'flowbite-react';
 
 function Navbarcode() {
-
     return (
         <>
             <header className="sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
@@ -58,24 +57,24 @@ function Navbarcode() {
                             <div className="flex">
                                 <Dropdown label="Aulas" class="font-medium text-[#EFEFEF] w-full hover:text-gray-500" dismissOnClick={false}>
                                     <Dropdown label="Surf" placement="right" class="font-medium text-gray-500 w-full hover:text-gray-500" dismissOnClick={false}>
-                                        <Link to="/horario">
+                                        <Link to="/...">
                                             <Dropdown.Item class="text-gray-500 font-medium pl-4 pb-2 pt-2 pr-4 hover:bg-gray-100">
                                                 Individual
                                             </Dropdown.Item>
                                         </Link>
-                                        <Link to="/test">
+                                        <Link to="/...">
                                             <Dropdown.Item class="text-gray-500 font-medium pl-4 pb-2 pt-2 pr-4 hover:bg-gray-100">
                                                 Grupo
                                             </Dropdown.Item>
                                         </Link>
                                     </Dropdown>
                                     <Dropdown label="Bodyboard" placement="right" class="font-medium text-gray-500 w-full hover:text-gray-500" dismissOnClick={false}>
-                                        <Link to="/test">
+                                        <Link to="/...">
                                             <Dropdown.Item class="text-gray-500 font-medium pl-4 pb-2 pt-2 pr-4 hover:bg-gray-100">
                                                 Individual
                                             </Dropdown.Item>
                                         </Link>
-                                        <Link to="/te">
+                                        <Link to="/...">
                                             <Dropdown.Item class="text-gray-500 font-medium pl-4 pb-2 pt-2 pr-4 hover:bg-gray-100">
                                                 Grupo
                                             </Dropdown.Item>
@@ -86,12 +85,12 @@ function Navbarcode() {
 
                             <div className="flex">
                                 <Dropdown label="Material" class="font-medium text-[#EFEFEF] w-full hover:text-gray-500" dismissOnClick={false}>
-                                    <Link to="/test">
+                                    <Link to="/...">
                                         <Dropdown.Item class="text-gray-500 font-medium pl-4 pb-2 pt-2 hover:bg-gray-100">
                                             Surf
                                         </Dropdown.Item>
                                     </Link>
-                                    <Link to="/horario">
+                                    <Link to="/...">
                                         <Dropdown.Item class="text-gray-500 font-medium pl-4 pb-2 pt-2 pr-4 hover:bg-gray-100">
                                             Bodyboard
                                         </Dropdown.Item>
@@ -99,18 +98,23 @@ function Navbarcode() {
                                 </Dropdown>
                             </div>
 
+                            {/* <Link to="/login"> */}
                             <Link to="/login" className="flex items-center gap-x-2 font-medium text-[#EFEFEF] hover:text-gray-500 sm:my-6 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-gray-500">
                                 <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                                 </svg>
-                                Iniciar sessão
+                                Log in
                             </Link>
+                            {/* </Link> */}
+
+                            {/* <Link to="/signup"> */}
                             <Link to="/signup" className="flex items-center gap-x-2 font-medium text-[#EFEFEF] hover:text-gray-500 sm:border-l sm:border-gray-500 sm:my-6 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-gray-500" href="#">
                                 <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                                 </svg>
-                                Inscrever
+                                Sign up
                             </Link>
+                            {/* </Link> */}
                         </div>
                     </div>
                 </nav>
@@ -120,3 +124,140 @@ function Navbarcode() {
 }
 
 export default Navbarcode
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
+                                <button type="button" className="flex items-center w-full text-[#EFEFEF] hover:text-gray-500 font-medium dark:text-gray-400 dark:hover:text-gray-500" data-hs-dropdown>
+                                    Aulas
+                                    <svg className="ml-2 w-2.5 h-2.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
+                                    </svg>
+                                </button>
+                                <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
+
+                                    <div className="hs-dropdown relative [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
+                                        <button type="button" className="w-full flex justify-between w-full items-center text-sm text-gray-800 rounded-md py-2 px-3 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
+                                            Surf
+                                            <svg className="sm:-rotate-90 ml-2 w-2.5 h-2.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
+                                            </svg>
+                                        </button>
+
+                                        <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-2 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute sm:border before:-right-5 before:top-0 before:h-full before:w-5 top-0 right-full !mx-[10px]">
+                                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                                Individual
+                                            </a>
+                                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                                Grupo
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                    <div className="hs-dropdown relative [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
+                                        <button type="button" className="w-full flex justify-between w-full items-center text-sm text-gray-800 rounded-md py-2 px-3 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
+                                            Bodyboard
+                                            <svg className="sm:-rotate-90 ml-2 w-2.5 h-2.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
+                                            </svg>
+                                        </button>
+
+                                        <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-2 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute sm:border before:-right-5 before:top-0 before:h-full before:w-5 top-0 right-full !mx-[10px]">
+                                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                                Individual
+                                            </a>
+                                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                                Grupo
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> */}
+{/* <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
+                                <button type="button" className="flex items-center w-full text-[#EFEFEF] hover:text-gray-500 font-medium dark:text-gray-400 dark:hover:text-gray-500">
+                                    Material
+                                    <svg className="ml-2 w-2.5 h-2.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
+                                    </svg>
+                                </button>
+                                <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
+                                    <div className="hs-dropdown relative [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
+                                        <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                            Surf
+                                        </a>
+                                        <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                            Bodyboard
+                                        </a>
+                                    </div>
+                                </div>
+                            </div> */}
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var dropdownButton = document.getElementById('dropdown-button');
+//     var isDropdownOpen = false;
+
+//     dropdownButton.addEventListener('click', function () {
+//         isDropdownOpen = !isDropdownOpen;
+//         var openIcon = document.querySelector('.hs-collapse-open');
+//         var closedIcon = document.querySelector('.hs-collapse-open:hidden');
+
+//         if (isDropdownOpen) {
+//             openIcon.classList.remove('hidden');
+//             closedIcon.classList.add('hidden');
+//             // Add code to show the dropdown content here
+//         } else {
+//             openIcon.classList.add('hidden');
+//             closedIcon.classList.remove('hidden');
+//             // Add code to hide the dropdown content here
+//         }
+//     });
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var menuButton = document.getElementById('menu-button');
+//     var isMenuOpen = false;
+
+//     menuButton.addEventListener('click', function () {
+//         isMenuOpen = !isMenuOpen;
+//         var openIcon = document.querySelector('.hs-collapse-open');
+//         var closedIcon = document.querySelector('.hs-collapse-open:hidden');
+
+//         if (isMenuOpen) {
+//             openIcon.classList.remove('hidden');
+//             closedIcon.classList.add('hidden');
+//             // Add code to show the menu or trigger any desired action here
+//         } else {
+//             openIcon.classList.add('hidden');
+//             closedIcon.classList.remove('hidden');
+//             // Add code to hide the menu or trigger any desired action here
+//         }
+//     });
+// });
